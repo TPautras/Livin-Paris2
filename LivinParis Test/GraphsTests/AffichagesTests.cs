f2d39b4e3b76e60e6961564e282ff0007b1a1c60
TPautras <155535273+TPautras@users.noreply.github.com>
2025-02-09 23:06:31 +0100

    add : GrapheTests

---
+++ LivinParis Test/GraphsTests/AffichagesTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Graphs;
+
+namespace LivinParis_Test
+{
+    [TestFixture]
+    public class AffichagesTests
+    {
+        [Test]
+        public void Banner_ShouldReturnNonEmptyString()
+        {
+            var result = Affichages.Banner();
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Length > 0);
+        }
+    }
+}
