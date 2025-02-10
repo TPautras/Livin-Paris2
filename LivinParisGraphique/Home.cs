cc407979a63c7c604df7a3d09d08e32475a26e18
TPautras <155535273+TPautras@users.noreply.github.com>
2025-02-10 22:05:21 +0100

    add : basic form

---
+++ LivinParisGraphique/Home.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LivinParisGraphique
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($@"Hello {textBox1.Text}");
+        }
+    }
+}
