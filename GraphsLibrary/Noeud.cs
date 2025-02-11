f7174d73d875bc069804db82c18a9fd011cd657e
TPautras <tpautras@gmail.com>
2025-02-11 17:46:04 +0100

    save

---
+++ GraphsLibrary/Noeud.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graphs
+{
+    public class Noeud<T>
+    {
+        public T Noeud_Valeur { get; set; }
+        public int Noeud_id { get; set; }
+        public List<Lien<T>> Liens { get; set; }
+
+        public Noeud(int id, T valeur = default)
+        {
+            Noeud_id = id;
+            Noeud_Valeur = valeur;
+            Liens = new List<Lien<T>>();
+        }
+
+        public override string ToString()
+        {
+            string res = $"L'id de ce noeud est : {Noeud_id}\n";
+            if (Noeud_Valeur != null)
+            {
+                res += $"Sa valeur est {Noeud_Valeur}\n";
+            }
+            foreach (Lien<T> l in Liens)
+            {
+                res += l.ToString() + "\n";
+            }
+            return res;
+        }
+    }
+}
---
+++ LivinParis Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Graphs;
+
+namespace LivinParis_Console
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Exercice1.Exo1();
+        }
+    }
+}
