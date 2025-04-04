58f4f7bc1c1a65e4ddf9abfdc7b329e15de77f3b
TPautras <155535273+TPautras@users.noreply.github.com>
2025-04-04 03:20:56 +0200

    add: Commande Listing

--- LivinParis Console/src/Interfaces/Modules/ModuleCommande.cs
+++ LivinParis Console/src/Interfaces/Modules/ModuleCommande.cs
@@ -15,8 +15,61 @@ namespace LivinParis_Console.Modules
     {
         public void ModuleCommandMain()
         {
-            Console.WriteLine("Module Commandes");
+            string[] options =
+            {
+                "Lister toutes les commandes", "Ajouter une commande",
+                "Quitter"
+            };
+            string prompt = "ADMINISTRATOR | Module Commande";
+            int adminChoice = Affichages.MenuSelect(prompt, options);
+            while (adminChoice < options.Length - 1)
+            {
+                switch (adminChoice)
+                {
+                    case 0:
+                        ModuleCommandeListing();
+                        Console.ReadKey();
+                        break;
+                    case 1:
+                        ModuleCommandeAdding();
+                        Console.ReadKey();
+                        break;
+                    default:
+                        break;
+                }
+
+                adminChoice = Affichages.MenuSelect(prompt, options);
+
+            }
         }
-        
+        private void ModuleCommandeAdding()
+        {
+            Console.WriteLine("Ajouter une commande");
+        }
+
+        private void ModuleCommandeListing()
+        {
+            Console.WriteLine("ADMINISTRATOR | Module Commande | LISTING");
+            List<Commande> commandeListe = new CommandeDataAccess().GetAll();
+            List<Plat> platListe = new PlatDataAccess().GetAll();
+            List<Creation> creationListe = new CreationDataAccess().GetAll();
+            foreach (Commande commande in commandeListe)
+            {
+                Console.WriteLine("Commande n :"+commande.CommandeId);
+                Console.WriteLine("Client : " + commande.ClientUsername);
+                Console.WriteLine("Cuisinier : " + commande.CuisinierUsername);
+                Console.WriteLine("Plats commandes : ");
+                foreach (Creation creation in creationListe)
+                {
+                    if (creation.CommandeId == commande.CommandeId)
+                    {
+                        Console.WriteLine(new RecetteDataAccess().GetById(new PlatDataAccess().GetById(creation.PlatId).RecetteId).RecetteNom);
+                        Console.WriteLine("Prix : " + new PlatDataAccess().GetById(creation.PlatId).PlatPrix);
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
     }
 }
