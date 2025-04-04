9c945fc64a77c2d79b90690fa236febe33d9ed0e
Cancouille <candice.alev@edu.devinci.fr>
2025-04-04 11:23:06 +0200

    Add : test unitaire

--- LivinParis Test/Parcours Tests/AStar.cs
+++ LivinParis Test/Parcours Tests/AStar.cs
@@ -2,6 +2,121 @@
 {
     public class AStar
     {
+        [TestFixture]
+        public class AStarTests 
+        {
+        [Test]
+        public void AStarTestChemin()
+        {
+            char divider = ' ';
+            int maxCount = 35;
+            string path = "../../soc-karate.mtx";
+            
+            // Créer le graphe à partir du fichier
+            Graphe<int> Graph = new Graphe<int>(path, divider, maxCount);
+            
+            // Créer l'instance de AStar
+            AStar<int> aStar = new AStar<int>(Graph);
+            
+            // Trouver le chemin le plus court entre les noeuds 1 et 6
+            (double distance, List<int> chemin) = aStar.TrouverChemin(1, 6);
+            
+            // Vérifier que le nœud d'arrivée est bien présent dans le chemin
+            Assert.That(chemin.Contains(6), Is.True);
+            
+            // Vérifier que le nœud de départ est bien présent dans le chemin
+            Assert.That(chemin.Contains(1), Is.True);
+            
+            // On peut aussi vérifier que le chemin est correctement ordonné
+            Assert.That(chemin[0], Is.EqualTo(1)); // Premier élément est le départ
+            Assert.That(chemin[chemin.Count - 1], Is.EqualTo(6)); // Dernier élément est l'arrivée
+        }
+
+        [Test]
+        public void AStarTestDistance()
+        {
+            char divider = ' ';
+            int maxCount = 35;
+            string path = "../../soc-karate.mtx";
+            
+            // Créer le graphe à partir du fichier
+            Graphe<int> Graph = new Graphe<int>(path, divider, maxCount);
+            
+            // Créer l'instance de AStar
+            AStar<int> aStar = new AStar<int>(Graph);
+            
+            // Trouver le chemin le plus court entre les noeuds 1 et 6
+            (double distance, List<int> chemin) = aStar.TrouverChemin(1, 6);
+            
+            // Vérifier que la distance est celle attendue (comme pour Dijkstra et Floyd-Warshall)
+            Assert.That(distance, Is.EqualTo(1));
+        }
         
+        [Test]
+        public void AStarTestCheminImpossible()
+        {
+            char divider = ' ';
+            int maxCount = 35;
+            string path = "../../soc-karate.mtx";
+            
+            // Créer le graphe à partir du fichier
+            Graphe<int> Graph = new Graphe<int>(path, divider, maxCount);
+            
+            // Créer l'instance de AStar
+            AStar<int> aStar = new AStar<int>(Graph);
+            
+            // Nœud qui n'existe pas dans le graphe
+            int noeudInexistant = maxCount + 1;
+            
+            // Tester avec try/catch pour les noeuds inexistants
+            try
+            {
+                (double distance, List<int> chemin) = aStar.TrouverChemin(1, noeudInexistant);
+                Assert.Fail("Une exception aurait dû être levée pour un noeud inexistant");
+            }
+            catch (ArgumentException)
+            {
+                // C'est le comportement attendu
+                Assert.Pass();
+            }
+        }
+        
+        [Test]
+        public void AStarTestCheminComplet()
+        {
+            char divider = ' ';
+            int maxCount = 35;
+            string path = "../../soc-karate.mtx";
+            
+            // Créer le graphe à partir du fichier
+            Graphe<int> Graph = new Graphe<int>(path, divider, maxCount);
+            
+            // Créer l'instance de AStar
+            AStar<int> aStar = new AStar<int>(Graph);
+            
+            // Trouver le chemin le plus court entre les noeuds 1 et 6
+            (double distance, List<int> chemin) = aStar.TrouverChemin(1, 6);
+            
+            // Vérifier que le chemin est cohérent - chaque nœud doit être connecté au suivant
+            for (int i = 0; i < chemin.Count - 1; i++)
+            {
+                int noeudActuel = chemin[i];
+                int noeudSuivant = chemin[i + 1];
+                
+                // Vérifier que noeudActuel est bien connecté à noeudSuivant
+                bool estConnecte = false;
+                foreach (var lien in Graph.Noeuds[noeudActuel].Liens)
+                {
+                    if (lien.LienArrivee.Noeud_id == noeudSuivant)
+                    {
+                        estConnecte = true;
+                        break;
+                    }
+                }
+                
+                Assert.That(estConnecte, Is.True, $"Le noeud {noeudActuel} n'est pas connecté au noeud {noeudSuivant}");
+            }
+        }
+    }
     }
 }
--- LivinParis Test/Parcours Tests/Floyd-Warshall.cs
+++ LivinParis Test/Parcours Tests/Floyd-Warshall.cs
@@ -2,6 +2,85 @@
 {
     public class Floyd_Warshall
     {
-        
+        [TestFixture]
+        public class FloydWarshallTests
+        {
+            [Test]
+            public void FloydWarshallTestChemin()
+            {
+                char divider = ' ';
+                int maxCount = 35;
+                string path = "../../soc-karate.mtx";
+
+                // Créer le graphe à partir du fichier
+                Graphe<int> Graph = new Graphe<int>(path, divider, maxCount);
+
+                // Créer l'instance de FloydWarshall
+                FloydWarshall<int> floydWarshall = new FloydWarshall<int>(Graph);
+
+                // Trouver le chemin le plus court entre les noeuds 1 et 6
+                (double distance, List<int> chemin) = floydWarshall.TrouverPlusCourtChemin(1, 6);
+
+                // Liste attendue pour le chemin
+                List<int> expected = new List<int>();
+                // Pour Floyd-Warshall, vous pourriez avoir un chemin différent de Dijkstra
+                // Le chemin devrait contenir au moins l'arrivée, et peut-être des noeuds intermédiaires
+                expected.Add(6);
+
+                // Vérifier que le chemin est celui attendu
+                Assert.That(chemin, Is.EqualTo(expected));
+            }
+
+            [Test]
+            public void FloydWarshallTestDistance()
+            {
+                char divider = ' ';
+                int maxCount = 35;
+                string path = "../../soc-karate.mtx";
+
+                // Créer le graphe à partir du fichier
+                Graphe<int> Graph = new Graphe<int>(path, divider, maxCount);
+
+                // Créer l'instance de FloydWarshall
+                FloydWarshall<int> floydWarshall = new FloydWarshall<int>(Graph);
+
+                // Trouver le chemin le plus court entre les noeuds 1 et 6
+                (double distance, List<int> chemin) = floydWarshall.TrouverPlusCourtChemin(1, 6);
+
+                // Vérifier que la distance est celle attendue
+                // Puisque Dijkstra a donné 1, on s'attend à la même distance pour Floyd-Warshall
+                Assert.That(distance, Is.EqualTo(1));
+            }
+
+            [Test]
+            public void FloydWarshallTestCheminImpossible()
+            {
+                char divider = ' ';
+                int maxCount = 35;
+                string path = "../../soc-karate.mtx";
+
+                // Créer un graphe où certains chemins sont impossibles
+                Graphe<int> Graph = new Graphe<int>(path, divider, maxCount);
+
+                // On peut tester avec un noeud qui n'existe pas dans le graphe
+                // ou deux noeuds qui ne sont pas connectés (selon la structure du graphe)
+                int noeudInexistant = maxCount + 1; // Noeud qui n'existe pas
+
+                // Créer l'instance de FloydWarshall
+                FloydWarshall<int> floydWarshall = new FloydWarshall<int>(Graph);
+
+                // Tester avec try/catch pour les noeuds inexistants
+                try
+                {
+                    (double distance, List<int> chemin) = floydWarshall.TrouverPlusCourtChemin(1, noeudInexistant);
+                    Assert.Fail("Une exception aurait dû être levée pour un noeud inexistant");
+                }
+                catch (ArgumentException)
+                {
+                    // C'est le comportement attendu
+                    Assert.Pass();
+                }
+            }
+        }
     }
 }
