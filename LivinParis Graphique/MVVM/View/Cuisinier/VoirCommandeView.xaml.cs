99b8048a35ae717ebefd89aa15c00aef779f59a3
TPautras <155535273+TPautras@users.noreply.github.com>
2025-05-04 14:53:14 +0200

    feat: add commands display for cook

---
+++ LivinParis Graphique/MVVM/View/Cuisinier/VoirCommandeView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LivinParis_Graphique.MVVM.View
+{
+    public partial class VoirCommandeView : UserControl
+    {
+        public VoirCommandeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
